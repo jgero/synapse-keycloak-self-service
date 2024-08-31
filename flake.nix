2c898d10622f98b1136070369ab7598c740d7339
Johannes Gerold <git+commit@jgero.me>
2024-08-31 21:42:14 +0200

    fix(image): tag with version only

--- flake.nix
+++ flake.nix
@@ -41,7 +41,7 @@
         };
         default = pkgs.dockerTools.buildLayeredImage {
           name = "ghcr.io/jgero/${bin.pname}";
-          tag = "latest";
+          tag = "${bin.version}";
           contents = with pkgs; [ cacert ];
           maxLayers = 10;
           config = {
